5167dc98337cdb3693eaa3269061a80221b6b3db
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-04 23:22:20 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Create route handler
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+
+// Create route handler
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
