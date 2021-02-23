aa65b9e2f3cf030c88455b8777d238412914756d
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-22 16:45:28 -0800

    Add passport setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 // Dynamically add port
 const PORT = process.env.PORT || 5000;
